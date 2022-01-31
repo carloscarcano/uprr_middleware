11bab42b9fe2594a0297f1f56f3e70ce3a745448
Carlos Carcaño <carlos.carcano@hotmail.com>
2022-01-31 12:24:20 -0600

    creación de servicio para carga de documento

--- src/mainController.js
+++ src/mainController.js
@@ -7,6 +7,25 @@ var checkStatus = function(req, res)
     res.json(result);
 }
 
+var cargaDocumento = function(req, res)
+{
+    var mensaje = "datos cargados correctamente";
+    var { nombre, apellidoPaterno, apellidoMaterno, grupo, materia, comunidad, coleccion } = req.body;
+    
+    if (esNulo(nombre) || esNulo(apellidoPaterno) || esNulo(apellidoMaterno) || esNulo(grupo) || esNulo(materia) || esNulo(comunidad) || esNulo(coleccion)) 
+    {
+        mensaje = 'faltan datos para poder realizar el proceso';
+        res.json({ mensaje: mensaje });
+        return false;
+    }
+}
+
+function esNulo(variable)
+{
+    return variable == null;
+}
+
 module.exports = {
-    checkStatus
+    checkStatus,
+    cargaDocumento
 }
--- src/routes.js
+++ src/routes.js
@@ -5,13 +5,6 @@ const mainController = require('./mainController');
 var router = express.Router();
 
 router.get('/check', mainController.checkStatus);
-
-// tickets
-/*
-router.post('/ticket', ticketController.agregarTicket);
-router.get('/ticket/:idTicket', ticketController.obtenerTicket);
-router.patch('/ticket/:idTicket', ticketController.modificarTicket);
-router.delete('/ticket/:idTicket', ticketController.eliminarTicket);
-*/
+router.post('/documento', mainController.cargaDocumento);
 
 module.exports = router;
